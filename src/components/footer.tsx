7d326c1cfa49e68e3ac34d18d5483ad195809708
Gabriel Carniel <gpcarniel@outlook.com>
2025-10-01 18:29:02 -0300

    correcao estilo do footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="border-t p-8  ">
-      <p className="text-center container max-w-[1366px] min-w-[414px] mx-auto font-montserrat text-muted">
+    <footer className="border-t p-6">
+      <p className="text-center container max-w-[1366px] min-w-[414px] mx-auto font-montserrat text-muted text-base leading-5">
         {new Date().getFullYear()} © Todos os direitos reservados a{' '}
-        <span className="font-bold">Cubos Movies</span>
+        <span className="font-semibold">Cubos Movies</span>
       </p>
     </footer>
   )
