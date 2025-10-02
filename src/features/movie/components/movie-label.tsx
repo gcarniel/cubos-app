099ee19b444b8008df59c7b887fd4c9d9c423f07
Gabriel Carniel <gpcarniel@outlook.com>
2025-10-02 16:53:51 -0300

    adiciona classname como prop ao componente movie-label

--- src/features/movie/components/movie-label.tsx
+++ src/features/movie/components/movie-label.tsx
@@ -1,11 +1,19 @@
+import { cn } from '@/lib/utils'
+
 interface MovieLabelProps {
   label: string
   value: string | number
+  className?: string
 }
 
-export function MovieLabel({ label, value }: MovieLabelProps) {
+export function MovieLabel({ label, value, className }: MovieLabelProps) {
   return (
-    <div className="flex flex-col gap-2 p-4 bg-[#232225BF] rounded-sm uppercase w-full h-fit">
+    <div
+      className={cn(
+        'flex flex-col gap-2 p-4 bg-[#232225BF] rounded-sm uppercase w-full h-fit',
+        className,
+      )}
+    >
       <span className="font-montserrat text-xs font-bold text-[#B5B2BC]">
         {label}
       </span>
