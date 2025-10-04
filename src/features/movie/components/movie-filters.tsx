785107330244742fc30bb97d59870e1fdc1f46dd
Gabriel Carniel <gpcarniel@outlook.com>
2025-10-04 13:47:11 -0300

    movendo hooks para pasta correta

--- src/features/movie/components/movie-filters.tsx
+++ src/features/movie/components/movie-filters.tsx
@@ -7,7 +7,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
-import { useMovieFilters } from '@/features/hooks/use-movie-filters'
+import { useMovieFilters } from '@/features/movie/hooks/use-movie-filters'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { ShowError } from '@/components/show-error'
